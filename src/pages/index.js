e70cd5c5633f3bcc6da3ba2cc282a35b743b2b0c
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-05 17:09:40 +0800

    added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,15 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
-
-const nameStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '50vh',
-};
+import profilePic from '@site/static/img/profile_pic.png';
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
      <Layout title="Home">
        <div 
-       style={nameStyle}>
+       className={styles.me}>
+         <img className={styles.profilePic} src={profilePic} />
          <h1> Kevin Loh Jun Yong </h1>
        </div>
      </Layout>
