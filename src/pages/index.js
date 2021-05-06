99fddcfba00dc316f5ac0c2ed4ff2b2b10310813
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-06 20:38:37 +0800

    added animated typing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
@@ -6,15 +6,30 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import profilePic from '@site/static/img/profile_pic.png';
+import Typist from "react-typist";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    console.log("Count: " + count);
+    setCount(1);
+  }, [count]);
   return (
      <Layout title="Home">
        <div 
-       className={styles.me}>
+       className={styles.center}>
          <img className={styles.profilePic} src={profilePic} />
          <h1> Kevin Loh Jun Yong </h1>
+         {count ? (
+        <Typist avgTypingDelay={50} onTypingDone={() => setCount(0)} className={styles.description}>
+          <span> I'm a CS undergrad at NUS! </span>
+          <Typist.Backspace count={22} delay={500} />
+          <span> software engineer </span>
+        </Typist>
+      ) : (
+        ""
+      )}
        </div>
      </Layout>
   );
