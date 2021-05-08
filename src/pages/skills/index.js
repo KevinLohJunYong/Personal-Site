bee6e7c1586417282cc10e1e5728617583e6b65f
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-08 23:57:58 +0800

    improved ui

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -2,26 +2,35 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import styles from '../styles.module.css';
+import { Divider } from "@react-md/divider";
 
 export default function Skills() {
     return (
-        <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center">
-            <Box color="white" bgcolor="palevioletred" p={1}>
-                   Languages
+        <div style={{marginLeft: '300px', marginTop: '250px', width: '50%' }}>
+            <h1 className={styles.words} style={{marginTop:'-60px',marginLeft:'300px'}}>
+                Skills
+            </h1>
+            <Box style={{'borderRadius':'20px'}} color="white" bgcolor="yellow" fontSize="larger">
+                   <Box p={2} style={{'borderRadius':'20px 0px 0px 20px'}} bgcolor="blue" width="50%">
+                      Languages
+                   </Box>
             </Box>
-            <Box color="white" bgcolor="palevioletred" p={1}>
-                   FrontEnd
+            <Box style={{'borderRadius':'20px',marginTop:'10px'}} color="white" bgcolor="yellow" fontSize="larger">
+                   <Box p={2} style={{'borderRadius':'20px 0px 0px 20px'}} bgcolor="blue" width="50%">
+                      FrontEnd
+                   </Box>
             </Box>
-            <Box color="white" bgcolor="palevioletred" p={1}>
-                   BackEnd
+            <Box style={{'borderRadius':'20px',marginTop:'10px'}} color="white" bgcolor="yellow" fontSize="larger">
+                   <Box p={2} style={{'borderRadius':'20px 0px 0px 20px'}} bgcolor="blue" width="50%">
+                      BackEnd
+                   </Box>
             </Box>
-            <Box color="white" bgcolor="palevioletred" p={1}>
-                   DataBases
+            <Box style={{'borderRadius':'20px',marginTop:'10px'}} color="white" bgcolor="yellow" fontSize="larger">
+                   <Box p={2} style={{'borderRadius':'20px 0px 0px 20px'}} bgcolor="blue" width="50%">
+                      DataBase
+                   </Box>
             </Box>
-      </Grid>
+      </div>
     );
 }
