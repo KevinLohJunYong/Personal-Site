4349d41a5ad8b0bc09754cda1ef02aaaf3a83140
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-09 22:19:58 +0800

    improved ui

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -4,33 +4,35 @@ import { Grid } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import styles from '../styles.module.css';
 import { Divider } from "@react-md/divider";
+import { red } from '@material-ui/core/colors';
+
 
 export default function Skills() {
     return (
-        <div style={{marginTop: '250px',width: '50%' }}>
-            <h1 className={styles.words} style={{marginTop:'-60px'}}>
-                Skills
-            </h1>
-            <Box className={styles.outerBox}>
-                   <Box className={styles.innerBox}>
-                      Languages
-                   </Box>
+        <div style={{marginTop:"100px"}}>
+        <h1 style={{color:"white",textAlign:"center"}}>
+            Skills
+        </h1>
+        <Box className={styles.outerBox}>
+            <Box className={styles.innerBox}>
+                Languages
             </Box>
-            <Box className={styles.outerBox}>
-                   <Box className={styles.innerBox}>
-                      FrontEnd
-                   </Box>
+        </Box>
+        <Box className={styles.outerBox} style={{marginTop:"10px"}}>
+            <Box className={styles.innerBox}>
+                FrontEnd
             </Box>
-            <Box className={styles.outerBox}>
-                   <Box className={styles.innerBox}>
-                      BackEnd
-                   </Box>
+        </Box>
+        <Box className={styles.outerBox} style={{marginTop:"10px"}}>
+            <Box className={styles.innerBox}>
+                BackEnd
             </Box>
-            <Box className={styles.outerBox}>
-                   <Box className={styles.innerBox}>
-                      DataBase
-                   </Box>
+        </Box>
+        <Box className={styles.outerBox} style={{marginTop:"10px"}}>
+            <Box className={styles.innerBox}>
+                DataBases
             </Box>
-      </div>
+        </Box>
+        </div>
     );
 }
