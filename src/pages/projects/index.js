97c904359aea006434539b110c9ea66ee4e7bf03
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-11 12:46:13 +0800

    done adding the card

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
+import Chip from "@material-ui/core/Chip";
 import codingWallPaper from '../../../static/img/codingWallPaper2.png';
 import portfolioWebSite from '../../../static/img/portfolioWebsite.png';
+import styles from '../styles.module.css';
 
 export default function Projects() {
     return (
@@ -16,11 +20,25 @@ export default function Projects() {
             position: "fixed",
             width: "100%"
           }}>
-              <Card style={{width:"30%",height:"40%",margin:"30px"}}>
+              <Card style={{width:"30%",height:"60%",margin:"30px"}}>
               <CardContent>
                       <CardMedia style={{height:"200px"}} image={portfolioWebSite}>
-                          
                       </CardMedia>
+                      <h1 style={{textAlign:"center",marginTop:"10px"}}>
+                          Portfolio Website
+                      </h1>
+                      <p style={{textAlign:"center"}}>
+                          The website you are viewing now! :D
+                      </p>
+                      <Button style={{marginLeft:"150px"}} size="large" variant="outlined" 
+                        href="https://github.com/KevinLohJunYong/Personal-Site" target= "_blank">
+                          Github
+                     </Button>
+                     <div style={{display:"flex",marginLeft:"40px",marginTop:"10px"}}>
+                     <Chip size="medium" label="ReactJS" variant="outlined" style={{margin:"10px"}} />
+                     <Chip size="medium" label="Docusaurus" variant="outlined" style={{margin:"10px"}} />
+                     <Chip size="medium" label="Material-UI" variant="outlined" style={{margin:"10px"}} />
+                     </div>
                   </CardContent>
               </Card>
         </div>
