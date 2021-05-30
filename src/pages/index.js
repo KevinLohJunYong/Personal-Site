a26ab390306f31ed78ed6fb413f0b77a77478660
KevinLohJunYong <e0439232@u.nus.edu>
2021-05-30 11:17:01 +0800

    made more mobile-friendly

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import profilePic from '@site/static/img/profile_pic.png';
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -32,13 +32,13 @@ export default function Projects() {
                       <p style={{textAlign:"center"}}>
                           The website you are viewing now! :D
                       </p>
-                      <Button className={styles.projectButton} size="large" variant="outlined" 
+                      <Button style={{marginLeft:getMarginLeftButton()}} size={getSizeButton()} variant="outlined" 
                         href="https://github.com/KevinLohJunYong/Personal-Site" target= "_blank">
                           Github
                      </Button>
                      <div className={styles.chips}>
-                     <Chip size="large" label="ReactJS" variant="outlined" style={{margin:"2%"}} />
-                     <Chip size="large" label="Material-UI" variant="outlined" style={{margin:"2%"}} />
+                     <Chip size={getSizeChips()} label="ReactJS" variant="outlined" style={{margin:"2%"}} />
+                     <Chip size={getSizeChips()} label="Material-UI" variant="outlined" style={{margin:"2%"}} />
                      </div>
                   </CardContent>
               </Card>
@@ -46,4 +46,13 @@ export default function Projects() {
         </div>
     </Layout>
     );
+}
+function getMarginLeftButton() {
+  return window.innerWidth <= 400 ? "20%" : "35%"
+}
+function getSizeButton() {
+  return window.innerWidth <= 400 ? "small" : "large"
+}
+function getSizeChips() {
+  return window.innerWidth <= 400 ? "small" : "large"
 }
